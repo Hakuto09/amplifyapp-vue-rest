d5d1a6a347884b8c0d0e0bbb6e095a76dfeeaf6a
Hakuto09 <bis2023a@gmail.com>
2024-05-18 13:45:19 +0900

    Change for Hkt 20240520r01-001. logo

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,30 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apib620dd96": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "amplifyappvueresta09368a4": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamo29652a67": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
