460d7cf125a0ce9151ae808d2ecd55c85682fbaa
Hakuto09 <bis2023a@gmail.com>
2024-05-22 15:19:54 +0900

    Change for Hkt 20240522r01-002_routing.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       id="nav" 
       class="tab-area-base">
       <ul class="tab-menu-base">
-　　　　　<!-- `router.js` で定義したルーティングルールとの紐付けを行っている -->
+        <!-- `router.js` で定義したルーティングルールとの紐付けを行っている -->
         <li><router-link to="/">Home</router-link></li>
         <!-- *** -->
         <!-- 省略 -->
