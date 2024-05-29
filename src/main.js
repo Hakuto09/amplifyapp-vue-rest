73eb353e34a4e41a028fcd35f4f31dcbf0006ff4
Hakuto09 <bis2023a@gmail.com>
2024-05-29 19:26:57 +0900

    20240529r02-015 front auth get-user

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { store } from './store'
+//import { store } from './store'
 
 import { Amplify } from "aws-amplify";
 import aws_exports from './aws-exports';
@@ -15,7 +15,7 @@ const app = createApp(App)
 console.log("main.js:", " After createApp(App): ");
 app.use(router)
 console.log("main.js:", " After app.use(router): ");
-app.use(store)
-console.log("main.js:", " After app.use(store): ");
+//app.use(store)
+//console.log("main.js:", " After app.use(store): ");
 app.mount('#app')
 console.log("main.js:", " After app.mount('#app'): ");
