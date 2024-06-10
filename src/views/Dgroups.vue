895832bf89bbc1447dd328ac7b9ce0823a9904a3
Hakuto09 <bis2023a@gmail.com>
2024-06-10 15:14:08 +0900

    20240610r01-027 front add new dgroup device

--- src/views/Dgroups.vue
+++ src/views/Dgroups.vue
@@ -143,12 +143,14 @@ export default {
 //    console.log(fileName, ":methods-function():", " this.account_id ", this.account_id);
 //    console.log(fileName, ":methods:", "In");
 
-    registerDgroup: function() {
+//    registerDgroup: function() {
+    const :registerDgroup = async () => {
       const funcName = [":methods:", "regsiterDgroup:"];
       console.log(fileName, funcName[0], funcName[1], "In.");
       console.log(fileName, funcName[0], funcName[1], " this.properties ", this.properties);
 
-      axios.post(url_base + '/dgroup',
+//      axios.post(url_base + '/dgroup',
+      await axios.post(url_base + '/dgroup',
         {
           dgroup_name: this.properties.message,
           account_id: userInfo.userId,
