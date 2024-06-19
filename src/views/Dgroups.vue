cfba3c5476ec34b945533f52022cccaee04c01b8
Hakuto09 <bis2023a@gmail.com>
2024-06-19 14:46:25 +0900

    20240619r01-011 front loginId

--- src/views/Dgroups.vue
+++ src/views/Dgroups.vue
@@ -362,7 +362,7 @@ export default {
       userId = userInfo.userId;
 //      console.log(fileName, funcName[0], funcName[1], "After await getCurrentUser():", " userInfo ", userInfo);
 //      console.log(fileName, funcName[0], funcName[1], "After await getCurrentUser():", " userInfo ", userInfo, " signInDetails ", signInDetails, " loginId ", loginId, " userId ", userId);
-      console.log(fileName, funcName[0], funcName[1], "After await getCurrentUser():", " userInfo ", userInfo, loginId ", loginId, " userId ", userId);
+      console.log(fileName, funcName[0], funcName[1], "After await getCurrentUser():", " userInfo ", userInfo, " loginId ", loginId, " userId ", userId);
 
       //    getCurrentUserlap();
 //    console.log(fileName, ":beforeCreate-function(): Mid.");
