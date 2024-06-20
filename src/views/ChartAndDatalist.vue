cb23b96e2d945a8381f76fc7f0c67dd4133c8f2f
Hakuto09 <bis2023a@gmail.com>
2024-06-20 14:47:41 +0900

    20240620r01-005 front deviceInfo for chart

--- src/views/ChartAndDatalist.vue
+++ src/views/ChartAndDatalist.vue
@@ -151,7 +151,7 @@ export default {
     const funcName = [":data:"];
     console.log(fileName, funcName[0], "In.");
 
-    console.log(fileName, ":data-function():", "Before return():");
+    console.log(fileName, funcName[0], "Before return():");
     return {
       data: {
         labels: [],
@@ -320,7 +320,7 @@ export default {
         this.device_id = deviceInfo.device_id;
         this.device_name = deviceInfo.device_name;
 //        console.log(fileName, funcName[0], funcName[1], "Before return:", " this.data ", this.data, " this.options ", this.options, " this.device_id ", this.device_id);
-        console.log(fileName, funcName[0], funcName[1], "Before return:", " this.data ", this.data, " this.options ", this.options, " this.device_id ", this.device_id," this.device_name ", this.device_name);
+        console.log(fileName, funcName[0], funcName[1], "Before return:", " this.data ", this.data, " this.options ", this.options, " this.device_id ", this.device_id," this.device_name ", this.device_name, " deviceInfo ", deviceInfo);
         return response;
       }
       catch (error) {
