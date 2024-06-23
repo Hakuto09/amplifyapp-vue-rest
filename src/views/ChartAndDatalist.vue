52d338239e3a98fb05bf00da2438760fda739a65
Hakuto09 <bis2023a@gmail.com>
2024-06-23 15:47:20 +0900

    20240621r01-022 front date-picker

--- src/views/ChartAndDatalist.vue
+++ src/views/ChartAndDatalist.vue
@@ -347,6 +347,7 @@ export default {
 
         const chartDataLocal = chartData;
         const chartOptionsLocal = chartOptions;
+        console.log(fileName, funcName[0], funcName[1], "After chartDataLocal and chartOptionsLocal", " chartDataLocal ", chartDataLocal, " chartOptionsLocal ", chartOptionsLocal);
         
 //        this.data = chartData;
         this.data = chartDataLocal;
