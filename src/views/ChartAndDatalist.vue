0d05e420d5c8fa936849ea1b2e0b0ea7ec8f9c75
Hakuto09 <bis2023a@gmail.com>
2024-06-23 15:41:47 +0900

    20240621r01-021 front date-picker

--- src/views/ChartAndDatalist.vue
+++ src/views/ChartAndDatalist.vue
@@ -148,6 +148,55 @@ let data1s = [];
 let date_start;
 let date_end;
 
+const chartData = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'data0',
+      yAxisID: "yleft",
+      backgroundColor: '#f87979',
+      fill: false,
+      borderWidth: 2,
+      borderColor: "rgba(2,63,138,0.8)",
+      pointBorderColor: "#fff",
+      pointBackgroundColor: "rgba(2,63,138,0.8)",
+      pointBorderWidth: 2,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "#1D5191",
+      pointHoverBorderColor: "#fff",
+      pointHoverBorderWidth: 2,
+      tension: 0,
+      data: data0s,
+    },
+    {
+      label: 'data1',
+      yAxisID: "yright",
+//          backgroundColor: '#f879f9',
+      backgroundColor: "#3A7AC9",
+      fill: false,
+      borderWidth: 2,
+      borderColor: "rgba(201,60,58,0.8)",
+      pointBorderColor: "#fff",
+      pointBackgroundColor: "rgba(201,60,58,0.8)",
+      pointBorderWidth: 2,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "#9A1B19",
+      pointHoverBorderColor: "#fff",
+      pointHoverBorderWidth: 2,
+      tension: 0,
+      data: data1s,
+    },
+/*
+    {
+      label: 'data2',
+      yAxisID: "yright",
+//          backgroundColor: '#f8f979',
+      backgroundColor: "#DB514E",
+      data: data2s,
+    },
+*/
+  ]
+};
 
 const chartOptions = {
   responsive: true,    // グラフのスクロール対応
@@ -220,7 +269,8 @@ export default {
 
     console.log(fileName, funcName[0], "Before return():");
     return {
-
+      data: null,
+/*
       data: {
 //        labels: [],
         labels: labels,
@@ -235,10 +285,10 @@ export default {
           },
         ],
       },
-
+*/
 //      data: chartData,
-//      options: null,
-      options: chartOptions,
+      options: null,
+//      options: chartOptions,
       device_id: '',
       device_name: '',
 //      date: date,
@@ -255,56 +305,6 @@ export default {
 
     currentInstance = getCurrentInstance();
     console.log(fileName, funcName[0], "After getCurrentInstance():", " currentInstance ", currentInstance);
-
-const chartData = {
-  labels: labels,
-  datasets: [
-    {
-      label: 'data0',
-      yAxisID: "yleft",
-      backgroundColor: '#f87979',
-      fill: false,
-      borderWidth: 2,
-      borderColor: "rgba(2,63,138,0.8)",
-      pointBorderColor: "#fff",
-      pointBackgroundColor: "rgba(2,63,138,0.8)",
-      pointBorderWidth: 2,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "#1D5191",
-      pointHoverBorderColor: "#fff",
-      pointHoverBorderWidth: 2,
-      tension: 0,
-      data: data0s,
-    },
-    {
-      label: 'data1',
-      yAxisID: "yright",
-//          backgroundColor: '#f879f9',
-      backgroundColor: "#3A7AC9",
-      fill: false,
-      borderWidth: 2,
-      borderColor: "rgba(201,60,58,0.8)",
-      pointBorderColor: "#fff",
-      pointBackgroundColor: "rgba(201,60,58,0.8)",
-      pointBorderWidth: 2,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "#9A1B19",
-      pointHoverBorderColor: "#fff",
-      pointHoverBorderWidth: 2,
-      tension: 0,
-      data: data1s,
-    },
-/*
-    {
-      label: 'data2',
-      yAxisID: "yright",
-//          backgroundColor: '#f8f979',
-      backgroundColor: "#DB514E",
-      data: data2s,
-    },
-*/
-  ]
-}
 
 //    async function getDeviceData(deviceId) {
 //    const getDeviceData = async (deviceId) => {
@@ -344,12 +344,17 @@ const chartData = {
           }
           console.log(fileName, funcName[0], funcName[1], ":After loop for chart data", " labels ", labels, " data0s ", data0s, " data1s ", data1s/*, " data2s ", data2s*/);
         }
+
+        const chartDataLocal = chartData;
+        const chartOptionsLocal = chartOptions;
         
-        this.data = chartData;
+//        this.data = chartData;
+        this.data = chartDataLocal;
 //        this.data.labels = labels;
 //        this.data.datasets[0].data = data0s;
 //        this.data.datasets[1].data = data1s;
 //        this.options = chartOptions;
+        this.options = chartOptionsLocal;
 
         //        this.device_id = deviceId;
 //        this.device_id = deviceInfo.device_id;
