b6934c27fb920267be93c2b8a95e5a5b636960f7
Hakuto09 <bis2023a@gmail.com>
2024-10-05 07:25:35 +0900

    20241007r01-001_map-view_via-1nce-api_get-1nce-api-token

--- src/views/DeviceDetail.vue
+++ src/views/DeviceDetail.vue
@@ -242,10 +242,22 @@ export default {
           },
         });
 */
+/*
         const response = await axios.post('https://api.1nce.com/management-api/oauth/token', {
             data: {
               'grant_type': 'client_credentials'
             },
+          }, {
+            headers: {
+              'accept': 'application/json',
+              'authorization': 'Basic bWFydXlhbWEtaEBoYWt1dG8uY28uanA6SGFrdXRvODkxMA==',
+              'content-type': 'application/json',
+            },
+          },
+        );
+*/
+        const response = await axios.post('https://api.1nce.com/management-api/oauth/token', {
+           'grant_type': 'client_credentials',
           }, {
             headers: {
               'accept': 'application/json',
