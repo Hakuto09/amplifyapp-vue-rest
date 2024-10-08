6151ddf93630889a1fe293a447258be64c3b655f
Hakuto09 <bis2023a@gmail.com>
2024-10-08 11:34:16 +0900

    20241008r01-003_get-1nce-params_via-1nce-api_usability

--- src/views/DeviceDetail.vue
+++ src/views/DeviceDetail.vue
@@ -65,6 +65,12 @@
     <br><br>
     <h2>1NCE Latest Devices Positions 情報</h2>
     <h3>(接続先の基地局位置情報)</h3>
+    <h1>This is heading 1</h1>
+    <h2>This is heading 2</h2>
+    <h3>This is heading 3</h3>
+    <h4>This is heading 4</h4>
+    <h5>This is heading 5</h5>
+    <h6>This is heading 6</h6>
     <!-- div id="map-container">
       <l-map id="map" :zoom="18" :center="[35.686541, 139.698851]">
         <l-tile-layer :url="url"></l-tile-layer>
