ec275faea7211fd90da12b20235cfacae5419de9
Hakuto09 <bis2023a@gmail.com>
2024-10-23 13:00:16 +0900

    20241023r01-006_aws-api-wait-and-disable-buttons

--- src/views/ChartAndDatalist - コピー.vue
+++ src/views/ChartAndDatalist - コピー.vue
@@ -562,8 +562,8 @@ export default {
 //        this.device_name = deviceInfo.device_name;
         this.device_name = deviceInfo['device_name'];
 
-//        this.inProgress = false;
-//        console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
+        this.inProgress = false;
+        console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
 
         //        console.log(fileName, funcName[0], funcName[1], "Before return:", " this.data ", this.data, " this.options ", this.options, " this.device_id ", this.device_id);
         console.log(fileName, funcName[0], funcName[1], "Before return:", " this.data ", this.data, " this.options ", this.options, " this.device_id ", this.device_id," this.device_name ", this.device_name, " deviceInfo ", deviceInfo);
@@ -573,8 +573,8 @@ export default {
       }
       catch (error) {
         console.error(fileName, funcName[0], funcName[1], "catch for axios.get(ddata_between):", " error ", error);
-//        this.inProgress = false;
-//        console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
+        this.inProgress = false;
+        console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
         return error;
       }
     }
@@ -614,9 +614,6 @@ export default {
 //    let response_ga = getDeviceData(deviceId.value);
     let response_ga = getDeviceData(deviceInfo.value, date_start_iso, date_end_iso);
     console.log(fileName, funcName[0], ":After getDeviceData()", " ddata ", ddata, " response_ga ", response_ga);
-
-    this.inProgress = false;
-    console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
   },
   created: function() {
     const funcName = [":created:"];
@@ -772,8 +769,8 @@ export default {
 //        this.device_name = deviceInfo.device_name;
         this.device_name = deviceInfo['device_name'];
 
-//        this.inProgress = false;
-//        console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
+        this.inProgress = false;
+        console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
 
         //        console.log(fileName, funcName[0], funcName[1], "Before return:", " this.data ", this.data, " this.options ", this.options, " this.device_id ", this.device_id);
         console.log(fileName, funcName[0], funcName[1], "Before return:", " this.data ", this.data, " this.options ", this.options, " this.device_id ", this.device_id," this.device_name ", this.device_name, " deviceInfo ", deviceInfo);
@@ -783,8 +780,8 @@ export default {
       }
       catch (error) {
         console.error(fileName, funcName[0], funcName[1], "catch for axios.get(ddata_between):", " error ", error);
-//        this.inProgress = false;
-//        console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
+        this.inProgress = false;
+        console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
         return error;
       }
     }
@@ -817,9 +814,6 @@ export default {
       let response_ga = getDeviceData(deviceInfo.value, date_start_iso, date_end_iso);
       console.log(fileName, funcName[0], ":After getDeviceData()", " ddata ", ddata, " response_ga ", response_ga);
     }
-
-    this.inProgress = false;
-    console.log(fileName, funcName[0], funcName[1], "After set this.inProgress:", " this.inProgress ", this.inProgress);
   },
   updated: function() {
     const funcName = [":updated:"];
